ba23782d040256d55165a0e0f6b8207d32eef546
Ayush Ganguly <ayushganguly@Ayushs-MacBook-Pro-2.local>
2022-04-29 16:34:12 +0400

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,12 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 //Global Middlewares
+
+//Implement CORS
+app.use(cors());
+//Access-Control-Allow-Origin *
+
+app.options('*', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, '/public')));
